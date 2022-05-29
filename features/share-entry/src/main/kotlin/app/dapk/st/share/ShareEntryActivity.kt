2ca96f21d26cf288e47ad8f85b5d7381730789b7
Adam Brown <adampsbrown@gmail.com>
2022-05-29 19:53:45 +0100

    adding barebones send intent handler activity

---
+++ features/share-entry/src/main/kotlin/app/dapk/st/share/ShareEntryActivity.kt
@@ -0,0 +1,51 @@
+package app.dapk.st.share
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Parcelable
+import app.dapk.st.core.DapkActivity
+
+class ShareEntryActivity : DapkActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val urisToShare = intent.readSendUrisOrNull() ?: throw IllegalArgumentException("")
+
+        // display list of rooms/converations
+
+//        homeViewModel.events.onEach {
+//            when (it) {
+//                HomeEvent.Relaunch -> recreate()
+//            }
+//        }.launchIn(lifecycleScope)
+//
+//        setContent {
+//            if (homeViewModel.hasVersionChanged()) {
+//                BetaUpgradeDialog()
+//            } else {
+//                HomeScreen(homeViewModel)
+//            }
+//        }
+    }
+}
+
+private fun Intent.readSendUrisOrNull(): List<Uri>? {
+    return when (this.action) {
+        Intent.ACTION_SEND -> {
+            if (this.hasExtra(Intent.EXTRA_STREAM)) {
+                listOf(this.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as Uri)
+            } else {
+                null
+            }
+        }
+        Intent.ACTION_SEND_MULTIPLE -> {
+            if (this.hasExtra(Intent.EXTRA_STREAM)) {
+                (this.getParcelableArrayExtra(Intent.EXTRA_STREAM) as Array<Uri>).toList()
+            } else {
+                null
+            }
+        }
+        else -> null
+    }
+}
