9899c5354854849aebf845401365e0136e13dd37
Adam Brown <adampsbrown@gmail.com>
2022-09-08 16:53:06 +0100

    using the REBASE automerge method to avoid syncing release back to main causing main to be ahead of release

--- tools/beta-release/app.js
+++ tools/beta-release/app.js
@@ -113,7 +113,7 @@ const enablePrAutoMerge = async (github, prNodeId) => {
         `,
         {
             pullRequestId: prNodeId,
-            mergeMethod: "MERGE"
+            mergeMethod: "REBASE"
         }
     )
 }
