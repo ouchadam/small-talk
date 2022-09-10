7554eac7afcf5d9492e731aeeebe1b2d80e8f6dc
Adam Brown <adampsbrown@gmail.com>
2022-09-10 17:10:48 +0100

    adding build script for generating signed foss releases

---
+++ tools/generate-fdroid-release.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+set -e
+
+WORKING_DIR=app/build/outputs/apk/release
+UNSIGNED=$WORKING_DIR/app-foss-release-unsigned.apk
+ALIGNED_UNSIGNED=$WORKING_DIR/app-foss-release-unsigned-aligned.apk
+SIGNED=$WORKING_DIR/app-foss-release-signed.apk
+
+ZIPALIGN=$(find "$ANDROID_HOME" -iname zipalign -print -quit)
+APKSIGNER=$(find "$ANDROID_HOME" -iname apksigner -print -quit)
+
+./gradlew clean assembleRelease -Pfoss -Punsigned --no-daemon --no-configuration-cache --no-build-cache
+
+$ZIPALIGN -v -p 4 $UNSIGNED $ALIGNED_UNSIGNED
+
+$APKSIGNER sign \
+  --ks .secrets/fdroid.keystore \
+  --ks-key-alias key0 \
+  --ks-pass pass:$1 \
+  --out $SIGNED \
+  $ALIGNED_UNSIGNED
