c8eaeff48f24df1a480ed2a63a2d3382267c2127
Adam Brown <adampsbrown@gmail.com>
2022-09-11 21:53:00 +0100

    only showing material you option for android 12+ devices

--- features/settings/src/main/kotlin/app/dapk/st/settings/SettingsItemFactory.kt
+++ features/settings/src/main/kotlin/app/dapk/st/settings/SettingsItemFactory.kt
@@ -1,26 +1,42 @@
 package app.dapk.st.settings
 
-import app.dapk.st.core.BuildMeta
-import app.dapk.st.core.ThemeStore
+import app.dapk.st.core.*
 import app.dapk.st.push.PushTokenRegistrars
 
 internal class SettingsItemFactory(
     private val buildMeta: BuildMeta,
+    private val deviceMeta: DeviceMeta,
     private val pushTokenRegistrars: PushTokenRegistrars,
     private val themeStore: ThemeStore,
 ) {
 
-    suspend fun root() = listOf(
+    suspend fun root() = general() + theme() + data() + account() + about()
+
+    private suspend fun general() = listOf(
         SettingItem.Header("General"),
         SettingItem.Text(SettingItem.Id.Encryption, "Encryption"),
         SettingItem.Text(SettingItem.Id.EventLog, "Event log"),
-        SettingItem.Text(SettingItem.Id.PushProvider, "Push provider", pushTokenRegistrars.currentSelection().id),
+        SettingItem.Text(SettingItem.Id.PushProvider, "Push provider", pushTokenRegistrars.currentSelection().id)
+    )
+
+    private fun theme() = listOfNotNull(
         SettingItem.Header("Theme"),
-        SettingItem.Toggle(SettingItem.Id.ToggleDynamicTheme, "Enable Material You", state = themeStore.isMaterialYouEnabled()),
+        SettingItem.Toggle(SettingItem.Id.ToggleDynamicTheme, "Enable Material You", state = themeStore.isMaterialYouEnabled()).takeIf {
+            deviceMeta.isAtLeastS()
+        },
+    ).takeIf { it.size > 1 } ?: emptyList()
+
+    private fun data() = listOf(
         SettingItem.Header("Data"),
         SettingItem.Text(SettingItem.Id.ClearCache, "Clear cache"),
+    )
+
+    private fun account() = listOf(
         SettingItem.Header("Account"),
         SettingItem.Text(SettingItem.Id.SignOut, "Sign out"),
+    )
+
+    private fun about() = listOf(
         SettingItem.Header("About"),
         SettingItem.Text(SettingItem.Id.PrivacyPolicy, "Privacy policy"),
         SettingItem.Text(SettingItem.Id.Ignored, "Version", buildMeta.versionName),
