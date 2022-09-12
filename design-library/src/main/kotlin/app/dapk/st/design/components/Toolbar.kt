e854e9b02c36d6129a6d56a304713d8391180761
Adam Brown <adampsbrown@gmail.com>
2022-09-12 20:57:33 +0100

    removing top bar under status bar

--- design-library/src/main/kotlin/app/dapk/st/design/components/Toolbar.kt
+++ design-library/src/main/kotlin/app/dapk/st/design/components/Toolbar.kt
@@ -1,17 +1,14 @@
 package app.dapk.st.design.components
 
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,7 +28,6 @@ fun Toolbar(
         title = title?.let { { Text(it, maxLines = 2) } } ?: {},
         actions = actions,
     )
-    Divider(modifier = Modifier.fillMaxWidth(), color = Color.Black.copy(alpha = 0.2f), thickness = 0.5.dp)
 }
 
 private fun foo(onNavigate: (() -> Unit)?): (@Composable () -> Unit) {
