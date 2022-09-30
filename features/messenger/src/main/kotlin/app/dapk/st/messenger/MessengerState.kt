6989c1399a870dfa7b27e17d5f324403406e2e7f
Adam Brown <adampsbrown@gmail.com>
2022-09-30 16:07:38 +0100

    adding swiping to start reply process

--- features/messenger/src/main/kotlin/app/dapk/st/messenger/MessengerState.kt
+++ features/messenger/src/main/kotlin/app/dapk/st/messenger/MessengerState.kt
@@ -2,6 +2,7 @@ package app.dapk.st.messenger
 
 import app.dapk.st.core.Lce
 import app.dapk.st.matrix.common.RoomId
+import app.dapk.st.matrix.sync.RoomEvent
 import app.dapk.st.navigator.MessageAttachment
 
 data class MessengerScreenState(
@@ -16,12 +17,16 @@ sealed interface MessengerEvent {
 
 sealed interface ComposerState {
 
+    val reply: RoomEvent?
+
     data class Text(
         val value: String,
+        override val reply: RoomEvent?,
     ) : ComposerState
 
     data class Attachments(
         val values: List<MessageAttachment>,
+        override val reply: RoomEvent?,
     ) : ComposerState
 
 }
