8a95a77d7aa3d4e014648ad3d5072438cb37c759
Adam Brown <adampsbrown@gmail.com>
2022-10-12 20:07:23 +0100

    porting settings tests to engine

--- chat-engine/src/testFixtures/kotlin/fake/FakeChatEngine.kt
+++ chat-engine/src/testFixtures/kotlin/fake/FakeChatEngine.kt
@@ -2,14 +2,18 @@ package fake
 
 import app.dapk.st.engine.ChatEngine
 import app.dapk.st.matrix.common.RoomId
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import test.delegateReturn
+import java.io.InputStream
 
 class FakeChatEngine : ChatEngine by mockk() {
 
     fun givenMessages(roomId: RoomId, disableReadReceipts: Boolean) = every { messages(roomId, disableReadReceipts) }.delegateReturn()
 
     fun givenDirectory() = every { directory() }.delegateReturn()
+
+    fun givenImportKeys(inputStream: InputStream, passphrase: String) = coEvery { inputStream.importRoomKeys(passphrase) }.delegateReturn()
 
 }
