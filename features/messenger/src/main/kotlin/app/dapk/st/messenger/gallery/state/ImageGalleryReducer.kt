6492afe06318963bd5188a545120330860403e9e
Adam Brown <adampsbrown@gmail.com>
2022-11-03 12:34:05 +0000

    adding missing image gallery reducer tests

--- features/messenger/src/main/kotlin/app/dapk/st/messenger/gallery/state/ImageGalleryReducer.kt
+++ features/messenger/src/main/kotlin/app/dapk/st/messenger/gallery/state/ImageGalleryReducer.kt
@@ -46,7 +46,6 @@ fun imageGalleryReducer(
                     parent = ImageGalleryPage.Routes.folders,
                     state = ImageGalleryPage.Files(Lce.Loading(), action.folder)
                 )
-
                 dispatch(PageAction.GoTo(page))
 
                 jobBag.replace(ImageGalleryPage.Files::class, coroutineScope.launch {
